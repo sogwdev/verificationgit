b3f3743985872665347bbf5d4735398bd27ff421
sogwdev <ogwdev01@gmail.com>
2021-05-03 13:48:47 +0900

    0503

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('https://verificationheroku.herokuapp.com/start', function () {
     return view('hello.hw');
 });
